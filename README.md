ed51978503b42d1332c6d44e221d2d37876e83ec
Sander Tolsma <github@tolsma.net>
2015-03-22 13:43:34 +0100

    First commit
    All initial scripts and markdown files

---
+++ README.md
@@ -0,0 +1,122 @@
+# Unattended TTC Ubuntu ISO Maker
+
+This simple script will create an unattended TTC Ubuntu ISO from start to finish. It will ask you a few questions once, and embed
+your answers into a remastered ISO file for you to use over and over again.
+
+This script creates a 100% original Ubuntu installation; no additional software is added, not even an ```apt-get update``` is
+performed. You have all the freedom in the world to customize your Ubuntu installation whichever way you see fit. This script
+just takes the pain out of re-installing Ubuntu over and over again.
+
+## Compatibility
+
+The script supports the following Ubuntu editions out of the box:
+
+* Ubuntu 12.04.4 Server LTS amd64 - Precise Pangolin
+* Ubuntu 14.04 Server LTS amd64 - Trusty Tahr
+
+This script has been tested on and with these two versions as well, but there is no reason why it shouldn't work with other Ubuntu
+editions. Other editions would require minor changes to the script though.
+
+## Usage
+
+* From your command line, run the following commands:
+
+```
+$ wget https://raw.githubusercontent.com/Tolsma/ubuntu-unattended/master/create-iso.sh
+$ chmod +x create-iso.sh
+$ sudo ./create-iso.sh
+```
+
+* Choose which version you would like to remaster:
+
+```
+ +---------------------------------------------------+
+ |            UNATTENDED UBUNTU ISO MAKER            |
+ +---------------------------------------------------+
+
+ which ubuntu edition would you like to remaster:
+
+  [1] Ubuntu 12.04.4 LTS Server amd64 - Precise Pangolin
+  [2] Ubuntu 14.04 LTS Server amd64 - Trusty Tahr
+
+ please enter your preference: [1|2]:
+```
+
+* Enter your github source location voor your config; the default is *https://github.com/Tolsma/ubuntu-unattended*:
+
+```
+ please enter your preferred timezone: https://github.com/Tolsma/ubuntu-unattended
+```
+
+* Enter your desired timezone; the default is *Europe/Amsterdam*:
+
+```
+ please enter your preferred timezone: Europe/Amsterdam
+```
+
+* Enter your desired username; the default is *sander*:
+
+```
+ please enter your preferred username: sander
+```
+
+* Enter the password for your user account; the default is *empty*
+
+```
+ please enter your preferred password:
+```
+
+* Confirm your password:
+
+```
+ confirm your preferred password:
+```
+
+* Sit back and relax, while the script does the rest! :)
+
+## What it does
+
+This script does a bunch of stuff, here's the quick walk-through:
+
+* It asks you for your preferences regarding the unattended ISO
+* Downloads the appropriate Ubuntu original ISO straight from the Ubuntu servers; if a file with the exact name exists, it will
+  use that instead (so it won't download it more than once if you are creating several unattended ISO's with different defaults)
+* Downloads the netson preseed file; this file contains all the magic answers to auto-install ubuntu. It uses the following
+  defaults for you (only showing most important, for details, simply check the seed file in this repository):
+ * Language/locale: en_US
+ * Keyboard layout: US International
+ * Root login disabled (so make sure you write down your default usernames' password!)
+ * Partitioning: LVM, full disk, single partition
+* Install the mkpasswd program (part of the whois package) to generate a hashed version of your password
+* Install the genisoimage program to generate the new ISO file
+* Mount the downloaded ISO image to a temporary folder
+* Copy the contents of the original ISO to a working directory
+* Set the default installer language
+* Add/update the preseed file
+* Add the autoinstall option to the installation menu
+* Generate the new ISO file
+* Cleanup
+* Show a summary of what happended:
+
+```
+ installing required packages
+ remastering your iso file
+ creating the remastered iso
+ -----
+ finished remastering your ubuntu iso file
+ the new file is located at: /tmp/ubuntu-14.04-server-amd64-unattended.iso
+ your username is: sander
+ your password is:
+ your hostname is: ubuntu
+ your timezone is: Europe/Amsterdam
+```
+
+### Once Ubuntu is installed...
+
+It fires off the start.sh script in your users' home directory to complete the installation. This will ask you all system info
+required to handle final system configuration.
+
+```$ sudo ~/start.sh```
+
+## License
+MIT
---
+++ start.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+set -e
+
+# set defaults
+default_hostname="hosting"
+default_domain="tolsma.net"
+
+#eth0
+default_eth0_ipv4="82.94.249.66"
+default_eth0_ipv4netmask="255.255.255.248"
+default_ipv4gateway="82.94.249.65"
+default_eth0_ipv6="2001:888:2000:47::4000:1"
+default_eth0_2ndipv6="2001:888:2000:47::4000:2"
+default_eth0_ipv6netmask="64"
+default_ipv6gateway="2001:888:2000:47::1"
+
+#eth1
+default_eth1_ipv4="10.128.0.150"
+default_eth1_ipv4netmask="255.255.255.0"
+
+# DNS resolvers
+default_nameservers="194.109.6.66 194.109.9.99"
+
+# temp location
+tmp="/home/sander/"
+
+clear
+
+# check for root privilege
+if [ "$(id -u)" != "0" ]; then
+   echo " this script must be run as root" 1>&2
+   echo
+   exit 1
+fi
+
+# define download function
+# courtesy of http://fitnr.com/showing-file-download-progress-using-wget.html
+download()
+{
+    local url=$1
+    echo -n "    "
+    wget --progress=dot $url 2>&1 | grep --line-buffered "%" | \
+        sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+    echo -ne "\b\b\b\b"
+    echo " DONE"
+}
+
+# determine ubuntu version
+ubuntu_version=$(lsb_release -cs)
+
+# check for interactive shell
+if ! grep -q "noninteractive" /proc/cmdline ; then
+    stty sane
+
+    # ask questions
+    read -ep " please enter your preferred hostname: " -i "$default_hostname" hostname
+    read -ep " please enter your preferred domain: " -i "$default_domain" domain
+
+    # eth0
+    read -ep " please enter the IPv4 address of eth0: " -i "$default_eth0_ipv4" eth0_ipv4
+    read -ep " please enter the IPv4 Netmask of eth0: " -i "$default_eth0_ipv4netmask" eth0_ipv4netmask
+    read -ep " please enter Default IPv4 Gateway (82.94.249.65/82.94.249.73): " -i "$default_ipv4gateway" ipv4gateway
+    read -ep " please enter the IPv6 address of eth0 (replace 4000): " -i "$default_eth0_ipv6" eth0_ipv6
+    read -ep " please enter the 2nd IPv6 address of eth0 (replace 4000): " -i "$default_eth0_2ndipv6" eth0_2ndipv6
+    read -ep " please enter the IPv6 Netmask of eth0: " -i "$default_eth0_ipv6netmask" eth0_ipv6netmask
+    read -ep " please enter Default IPv6 Gateway: " -i "$default_ipv6gateway" ipv6gateway
+
+    # eth1
+    read -ep " please enter the IP address of eth1: " -i "$default_eth0_ipv4" eth1_ipv4
+    read -ep " please enter the Netmask of eth1: " -i "$default_eth0_ipv4netmask" eth1_ipv4netmask
+
+    # DNS resolvers
+    read -ep " please enter Nameservers: " -i "$default_nameservers" nameservers
+
+fi
+
+# print status message
+echo " preparing your server; this may take a few minutes ..."
+
+# configure network
+file="/etc/network/interfaces"
+cat << EOF > $file
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+# The loopback network interface
+auto lo eth0 eth1
+iface lo inet loopback
+
+# Configure eth0 ipv4
+iface eth0 inet static
+address $eth0_ipv4
+netmask $eth0_ipv4netmask
+#broadcast 82.94.249.71
+#network 82.94.249.64
+gateway $ipv4gateway
+
+# dns-* options are implemented by the resolvconf package, if installed
+dns-search $domain
+dns-nameservers $nameservers
+
+# Configure etho ipv6
+iface eth0 inet6 static
+pre-up /sbin/modprobe -q ipv6 ; /bin/true
+address $eth0_ipv6
+netmask $eth0_ipv6netmask
+up ifconfig eth0 inet6 add $eth0_2ndipv6/$eth0_ipv6netmask
+gateway $ipv6gateway
+
+# The second network interface
+auto eth1
+iface eth1 inet static
+address $eth1_ip
+netmask $eth1_netmask
+
+EOF
+#don't use any space before of after 'EOF' in the previous line
+
+# set fqdn
+fqdn="$hostname.$domain"
+
+# update hostname
+echo "$hostname" > /etc/hostname
+sed -i "s@ubuntu.ubuntu@$fqdn@g" /etc/hosts
+sed -i "s@ubuntu@$hostname@g" /etc/hosts
+hostname "$hostname"
+
+# reset network interfaces to new config
+ifdown lo
+ifup lo
+ifdown eth0
+ifup eth0
+ifdown eth1
+ifup eth1
+
+# update repos
+apt-get -y update > /dev/null 2>&1
+apt-get -y upgrade > /dev/null 2>&1
+apt-get -y dist-upgrade > /dev/null 2>&1
+apt-get -y autoremove > /dev/null 2>&1
+apt-get -y purge > /dev/null 2>&1
+
+# install vmware tools package
+apt-get -y install open-vm-tools > /dev/null 2>&1
+
+# install all other standard packages
+
+# remove myself to prevent any unintended changes at a later stage
+rm $0
+
+# finish
+echo " DONE; rebooting ... "
+
+# reboot
+reboot
